341c9d5ece91de78dd220fd28dd5a84f49cc8313
TheJester32 <bogdanpopoveducation@gmail.com>
2023-07-11 01:31:56 +0300

    membership section and competitive section
    membership section and competitive section

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import './gym.js';
-
+import './membership.js';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
